739a91fbfae861c6fbe3f4ad2f0eed9161747a78
henrylai <henrylai4@gmail.com>
2016-04-20 12:59:28 -0700

    Added to FruitActivity

--- CookNow/app/src/main/java/com/example/tony/cooknow/FruitActivity.java
+++ CookNow/app/src/main/java/com/example/tony/cooknow/FruitActivity.java
@@ -2,12 +2,17 @@ package com.example.tony.cooknow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FruitActivity extends AppCompatActivity {
+
+    private TextView myPlaceholderText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fruit);
+        myPlaceholderText = (TextView) findViewById(R.id.PLACEHOLDERTEXT);
+        myPlaceholderText.append("akjfhsakjda");
     }
 }
